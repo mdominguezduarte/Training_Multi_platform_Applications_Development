23a96ae142863e87b82e1ddc7509beec17fe20a2
dominguezduarte <mdominguezduarte@gmail.com>
2020-12-01 14:58:59 +0100

    html and css

---
+++ html_css/css/periodic_table.css
@@ -0,0 +1,110 @@
+
+
+         
+         h1{
+            font-family: 'Sansita Swashed', cursive;
+            color:darkgray;
+            left:25%;
+            position:relative;
+            margin-bottom:7%;
+            text-shadow:0px 2px 0px rgb(140, 207, 229);  
+         }  
+         
+         td{
+            min-width: 60px;
+         }
+
+         .center{
+           
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+
+         }
+         button{
+            box-shadow: 2px 3px 0px 0px lightblue;
+	         background:linear-gradient(to bottom, #ededed 5%, #bab1ba 100%);
+	         background-color:#ededed;
+            border-radius:15px;
+            left:40%;
+            position:relative;
+            margin-top: 4%;
+	         border:1px solid #d6bcd6;
+	         display:inline-block;
+	         cursor:pointer;
+            color:gray;
+	         font-family:Arial;
+	         font-size:15px;
+	         padding:6px 15px;
+	         text-decoration:none;
+	         text-shadow:0px 1px 0px lightblue;
+         }
+         
+         a{
+            font-size:8px;
+         }
+         
+         body{
+            background-color:whitesmoke;
+            margin-left: 1em;
+         }
+         table{
+            border: 1px solid darkgray;
+         }
+
+         .empty{
+            background-color:whitesmoke;
+         }
+
+         .group_one{
+             background-color:#54478C;
+             color:whitesmoke;
+         }
+         .group_one:hover, .group_two:hover,.trans_metal:hover,.trans_metal:hover,.semi_metal:hover,
+         .metalloide:hover,.nonmetal:hover,.halogen:hover,.noble_gas:hover,.sel:hover{
+            background-color:pink;
+         }
+
+
+         .group_two{
+            background-color:#2C699A;
+            color:whitesmoke;
+
+         }
+         .trans_metal{
+            background-color: #048BA8;
+            color:whitesmoke;
+
+         }
+         .semi_metal{
+            background-color:#0DB39E;
+         }
+         .metalloide{
+            background-color: #16DB93;
+
+         }
+         .nonmetal{
+            background-color: #83E377;
+
+         }
+         .halogen{
+            background-color: #B9E769;
+
+         }
+         .noble_gas{
+            background-color: #EFEA5A;
+
+         }
+         .lacthanido{
+            background-color: #F1C453;
+            margin-top: 1em;
+
+
+         }
+         .actinido{
+            background-color: #F29E4C;
+
+         }
